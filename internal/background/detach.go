069cc071a27aaf5b40710dc7a964556651a1a06a
Nox1KCL <korshunov.maks09@gmail.com>
2026-06-17 16:42:16 +0300

    fix: replace hardcoded user lock path with OS cache dir

--- internal/background/detach.go
+++ internal/background/detach.go
@@ -2,6 +2,8 @@ package background
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/gofrs/flock"
 )
@@ -15,7 +17,15 @@ func RunInBackground(childArgs []string) error {
 }
 
 func IsChildRunning() (*flock.Flock, error) {
-	fileLock := flock.New("/home/nox/InFolderSort/app.lock")
+	cacheDir, err := os.UserCacheDir()
+	if err != nil {
+		return nil, fmt.Errorf("getting user cache dir: %w", err)
+	}
+	lockPath := filepath.Join(cacheDir, "Noxie-Sort", "app.lock")
+	if err := os.MkdirAll(filepath.Dir(lockPath), 0755); err != nil {
+		return nil, fmt.Errorf("creating lock dir: %w", err)
+	}
+	fileLock := flock.New(lockPath)
 	locked, err := fileLock.TryLock()
 	if err != nil {
 		return nil, err
